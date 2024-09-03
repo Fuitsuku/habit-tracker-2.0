91358ca84a09ebaf281a1e4ef2843488540c4047
Fuitsuku <mitsulearnscode@gmail.com>
2024-09-02 20:57:12 -0700

    Continued working on tabs
    Still need to build the body and change colors

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,5 +1,7 @@
 // Imports start here
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Checkbox } from "@/components/ui/checkbox"
 import PageHeader from "../components/PageHeader";
 
 export default function TasksPage() {
@@ -11,6 +13,20 @@ export default function TasksPage() {
                     <TabsTrigger value="current"> Current</TabsTrigger>
                     <TabsTrigger value="setup">Set Up</TabsTrigger>
                 </TabsList>
+                <TabsContent value="current">
+                    <Card>
+                        <CardContent className="space-y-2">
+                            
+                        </CardContent>
+                    </Card>
+                </TabsContent>
+                <TabsContent value="setup">
+                    <Card>
+                        <CardContent>
+
+                        </CardContent>
+                    </Card>
+                </TabsContent>
             </Tabs>
         </div>
     );
