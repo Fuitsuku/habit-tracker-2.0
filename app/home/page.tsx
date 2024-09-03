c6c3a24d4978d6794c90b4720dc1321a97dd310a
Fuitsuku <mitsulearnscode@gmail.com>
2024-09-02 18:21:45 -0700

    Home Page skeleton complete
    Still need to do routing and make buttons do things.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,32 @@
 //Imports start here
+import { Button } from "@/components/ui/button"
 
 export default function HomePage() {
     return (
-        <div>
-            
+        <div className="bg-zinc-900 min-h-screen p-10">
+            <h1 className="scroll-m-20  font-sans font-thin tracking-wide text-7xl text-white  tracking-tight lg:text-3xl">
+                Home
+            </h1>
+            <div className="text-white text-4xl">
+                ---
+            </div>
+            <ul className="py-10 space-y-3 list-outside">
+                <li>
+                    <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl p">
+                        Tasks
+                    </Button>
+                </li>
+                <li>
+                    <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl">
+                        Rewards
+                    </Button>
+                </li>
+                <li>
+                    <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl">
+                        Stats
+                    </Button>
+                </li>
+            </ul>
         </div>
     );
 }
