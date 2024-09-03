7633b6aea7a50ba3134b2fcd599662183384ce64
Fuitsuku <mitsulearnscode@gmail.com>
2024-09-02 18:31:10 -0700

    Created rewards page w/ header
    Body is still empty. Going to add tabs shadcn component

---
+++ app/rewards/page.tsx
@@ -0,0 +1,14 @@
+// Imports start here
+
+export default function RewardsPage() {
+    return (
+        <div className="bg-zinc-900 min-h-screen p-10">
+            <h1 className="scroll-m-20  font-sans font-thin tracking-wide text-7xl text-white  tracking-tight lg:text-3xl">
+                Rewards
+            </h1>
+            <div className="text-white text-4xl">
+                ---
+            </div>
+        </div>
+    );
+}
