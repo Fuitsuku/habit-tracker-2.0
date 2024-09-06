895be2f479f63cc95529a152d068bf8d64abc17f
Fuitsuku <mitsulearnscode@gmail.com>
2024-09-06 15:28:31 -0700

    Created task table skeleton w/ dummy data
    Still need to make the table update on change and eventually tie it to the back end. Will worry about that later down the road however.

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -2,7 +2,33 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+  
 import PageHeader from "../components/PageHeader";
+
+const test_task_data = [
+    {
+        task_id : 1,
+        completion : false,
+        task_name : "Task 1",
+        points : 5,
+        day_streak : 1
+    },
+    {
+        task_id : 2,
+        completion : false,
+        task_name : "Task 2",
+        points : 8,
+        day_streak : 1
+    },
+    {
+        task_id : 3,
+        completion : true,
+        task_name : "Task 3",
+        points : 15,
+        day_streak : 3
+    }
+]
 
 export default function TasksPage() {
     return (
@@ -16,7 +42,32 @@ export default function TasksPage() {
                 <TabsContent value="current">
                     <Card>
                         <CardContent className="space-y-2">
-                            
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead className="text-center">Status</TableHead>
+                                        <TableHead>Task</TableHead>
+                                        <TableHead className="text-center">Points</TableHead>
+                                        <TableHead className="text-center">Day Streak</TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    {
+                                        test_task_data.map(
+                                            (task) => (
+                                                <TableRow key={task.task_id}>
+                                                    <TableCell className="text-center">
+                                                        <Checkbox checked={task.completion}></Checkbox>
+                                                    </TableCell>
+                                                    <TableCell>{task.task_name}</TableCell>
+                                                    <TableCell className="text-center">{task.points}</TableCell>
+                                                    <TableCell className="text-center">{task.day_streak}</TableCell>
+                                                </TableRow>
+                                            )
+                                        )
+                                    }
+                                </TableBody>
+                            </Table>
                         </CardContent>
                     </Card>
                 </TabsContent>
