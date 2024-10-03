5d08c4e205dfab2afb3661b5410fb0f4b03d558c
Fuitsuku <mitsulearnscode@gmail.com>
2024-10-02 19:27:11 -0700

    Added back and potential gain. Also fixed size of current task table

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -3,6 +3,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+import { Button } from "@/components/ui/button"
   
 import PageHeader from "../components/PageHeader";
 
@@ -27,8 +28,31 @@ const test_task_data = [
         task_name : "Task 3",
         points : 15,
         day_streak : 3
+    },
+    {
+        task_id : 4,
+        completion : false,
+        task_name : "Task 4",
+        points : 5,
+        day_streak : 1
+    },
+    {
+        task_id : 5,
+        completion : false,
+        task_name : "Task 5",
+        points : 8,
+        day_streak : 1
+    },
+    {
+        task_id : 6,
+        completion : true,
+        task_name : "Task 6",
+        points : 15,
+        day_streak : 3
     }
 ]
+
+const potential_gain = 10;
 
 export default function TasksPage() {
     return (
@@ -41,21 +65,21 @@ export default function TasksPage() {
                 </TabsList>
                 <TabsContent value="current">
                     <Card>
-                        <CardContent className="space-y-2">
-                            <Table>
-                                <TableHeader>
+                        <CardContent className="space-y-2 h-96 overflow-y-auto">
+                            <Table className="min-w-full">
+                                <TableHeader className="">
                                     <TableRow>
-                                        <TableHead className="text-center">Status</TableHead>
-                                        <TableHead>Task</TableHead>
-                                        <TableHead className="text-center">Points</TableHead>
-                                        <TableHead className="text-center">Day Streak</TableHead>
+                                        <TableHead className=" sticky top-0 z-10 text-center">Status</TableHead>
+                                        <TableHead className=" sticky top-0 z-10">Task</TableHead>
+                                        <TableHead className=" sticky top-0 z-10text-center">Points</TableHead>
+                                        <TableHead className=" sticky top-0 z-10text-center">Day Streak</TableHead>
                                     </TableRow>
                                 </TableHeader>
-                                <TableBody>
+                                <TableBody className="">
                                     {
                                         test_task_data.map(
                                             (task) => (
-                                                <TableRow key={task.task_id}>
+                                                <TableRow key={task.task_id} className="h-12">
                                                     <TableCell className="text-center">
                                                         <Checkbox checked={task.completion}></Checkbox>
                                                     </TableCell>
@@ -79,6 +103,22 @@ export default function TasksPage() {
                     </Card>
                 </TabsContent>
             </Tabs>
+
+            <div className="flex justify-between">
+                <div className="text-left">
+                    <Button className="bg-zinc-900">
+                        &lt;--- Back
+                    </Button>
+                </div>
+                <div className="text-white text-right flex flex-col">
+                    <div>
+                        Projected Gain:
+                    </div> 
+                    <div>
+                        {potential_gain} &#9650;
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
