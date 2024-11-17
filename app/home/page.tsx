e407d916075b5a581a6fea5f967f2dd1ead216e5
Fuitsuku <mitsulearnscode@gmail.com>
2024-11-16 20:58:35 -0700

    Updated home page Dynamically centers content when you resize now

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,25 +4,27 @@ import PageHeader from '../components/PageHeader';
 
 export default function HomePage() {
     return (
-        <div className="bg-zinc-900 min-h-screen p-10">
-            <PageHeader page_name="Home"/>
-            <ul className="space-y-3 list-outside">
-                <li>
-                    <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl p">
-                        Tasks
-                    </Button>
-                </li>
-                <li>
-                    <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl">
-                        Rewards
-                    </Button>
-                </li>
-                <li>
-                    <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl">
-                        Stats
-                    </Button>
-                </li>
-            </ul>
+        <div className="bg-zinc-900 flex justify-center h-screen p-10">
+            <div className="space-y-6 w-full max-w-md">
+                <PageHeader page_name="Home" />
+                <ul className="space-y-3 list-none">
+                    <li>
+                        <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl">
+                            Tasks
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl">
+                            Rewards
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl">
+                            Stats
+                        </Button>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
