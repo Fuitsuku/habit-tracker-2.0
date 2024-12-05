e3493d8d3d4aaaba7caf2faebd9b2f39c5e03723
Fuitsuku <mitsulearnscode@gmail.com>
2024-12-04 17:44:01 -0700

    Duplicated home page to start as a baseline. Will remove elements and add text fields + buttons for the login links

---
+++ app/login/page.tsx
@@ -0,0 +1,30 @@
+//Imports start here
+import { Button } from "@/components/ui/button"
+import PageHeader from '../components/PageHeader';
+
+export default function LoginPage() {
+    return (
+        <div className="bg-zinc-900 flex justify-center h-screen p-10">
+            <div className="space-y-6 w-full max-w-md">
+                <PageHeader page_name="Login" />
+                <ul className="space-y-3 list-none">
+                    <li>
+                        <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl">
+                            Tasks
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl">
+                            Rewards
+                        </Button>
+                    </li>
+                    <li>
+                        <Button className="bg-zinc-900 font-sans font-thin text-white text-4xl">
+                            Stats
+                        </Button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
